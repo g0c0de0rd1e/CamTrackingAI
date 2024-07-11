c000567c49cf1c1c307d7341b930b84285ed7a49
Valera <kqlykjk@gmail.com>
2024-07-11 22:39:02 +0300

    Full tracking

--- main.py
+++ main.py
@@ -32,4 +32,35 @@ fps = int(capture.get(cv2.CAP_PROP_FPS))
 width = int(capture.get(cv2.CAP_PROP_FRAME_WIDTH))
 height = int(capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
+while True: 
+    #Захват кадра 
+    ret, frame = capture.read()
+    if not ret: 
+        break 
 
+    # Обработка кадра при помощи модели 
+    results = model(frame)[0]
+
+    # Данные об объектах
+    classes_names = results.names
+    classes = results.boxes.cls.cpu().numpy()
+    boxes = results.boxes.xyxy.cpu().numpy().astype(np.int32)
+    # Рисование рамок и данных 
+    for class_id, box in zip(results.boxes.cls.cpu().numpy(),
+                             results.boxes.xyxy.cpu().numpy().astype(np.int32)):
+        class_name = results.names[int(class_id)]
+        x1, y1, x2, y2 = box
+        cv2.rectangle(frame, (x1, y1), (x2, y2), color, 2)
+        cv2.putText(frame,
+                    class_name,
+                    (x1, y1 - 10),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5,
+                    color, 2)
+
+    cv2.imshow('YOLOv8 Detection', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+capture.release()
+cv2.destroyAllWindows()
